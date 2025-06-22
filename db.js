7d2c4ef46666d7efcf756e925dceee5ed91f297b
Slava Mryasov <slavadevbox1@gmail.com>
2025-06-22 23:08:35 +0300

    fix: change data in db

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: 'admin',
-  host: 'dpg-d1c5hbgdl3ps73famolg-a.pg.render.com',
-  database: 'newHub',
+  user: 'newhub_user',
+  host: 'dpg-d1c5hbgdl3ps73famolg-a.oregon-postgres.render.com',
+  database: 'newhub',
   password: 'EqoVoukeD2O7JaORJH6uDCrwMynZkXFh',
   port: 5432,
   ssl: {
