7cb1fc1450cac9f314d2d91596468beacbe734ba
Slava Mryasov <slavadevbox1@gmail.com>
2025-06-29 15:42:41 +0300

    feat: work on back v2

--- index.js
+++ index.js
@@ -73,5 +73,5 @@ app.delete("/api/news", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
